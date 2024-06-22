3bec65de50a430d3dedfb983beafbcc8defd4333
2lay <mail@2lay.net>
2024-06-22 22:57:13 +0200

    feat: misc ui changes

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,11 +1,11 @@
 <template>
-    <div style="text-align: center;">
-        <h1 style="margin-bottom: 15px;">Contact <em style="text-decoration: line-through 4px;">us</em> me!</h1>
+    <div style="justify-content: center;">
+        <h1 style="margin-bottom: 15px;"># Contact <em style="text-decoration: line-through 4px;">us</em> me!</h1>
         <h2>You can contact me on Discord using the handle <strong>2lay</strong>, or by mailing me at
             <strong>mail@2lay.net</strong>
         </h2>
         <br>
-        <div style="display: flex; justify-content: center;">
+        <div style="">
             <NuxtLink to="https://github.com/2lay/mctools.net">
                 <mct-button icon-name="mdiGithub" label="Source code" class="mct-button-primary"></mct-button>
             </NuxtLink>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,23 @@
 <template>
-	<h1 style="text-align: center; margin-bottom: 15px;">McTools.net</h1>
-	<h2 style="text-align: center;">A website that offers essential tools altered for Minecraft Java server admins.</h2>
+	<div>
+		<div>
+			<h1 style="margin-bottom: 15px;"># McTools.net</h1>
+			<h2 style="margin-bottom: 35px;" class="bold">A website that offers essential tools altered for Minecraft
+				Java
+				server admins.
+			</h2>
+		</div>
+		<div>
+			<div style="text-align: center;">
+				<h2 style="margin-bottom: 10px; font-weight: 900 !important">Contributors
+				</h2>
+				<div style="margin-bottom: 15px;">
+					<img src="https://contrib.rocks/image?repo=2lay/mctools.net">
+				</div>
+				<NuxtLink to="https://github.com/2lay/mctools.net">
+					<mct-button icon-name="mdiGithub" label="Source code" class="mct-button-primary"></mct-button>
+				</NuxtLink>
+			</div>
+		</div>
+	</div>
 </template>
